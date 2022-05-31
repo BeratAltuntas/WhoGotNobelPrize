536289f038f2eabc448388d8693b6aa5108bf9e0
Berat Altuntaş <cberataltuntas@gmail.com>
2022-05-31 23:07:11 +0300

    update

--- WhoGotNobelPrize/WhoGotNobelPrize/Config.swift
+++ WhoGotNobelPrize/WhoGotNobelPrize/Config.swift
@@ -1,12 +1,8 @@
 //
-//  Config.swift
+//  config.swift
 //  WhoGotNobelPrize
 //
 //  Created by BERAT ALTUNTAŞ on 31.05.2022.
 //
 
 import Foundation
-
-final class Config {
-	static let endpoint = ""
-}
